ff979d6c21de3f1673bcccaa881ece27fadef3f0
sses40417 <sses40417@gmail.com>
2022-07-31 18:01:49 +0800

    加入專案檔案。

---
+++ YieldQuerySystem/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using YieldQuerySystem.Models;
+using YieldQuerySystem.Models.ViewModel;
+
+namespace YieldQuerySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDbConnection _conn;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController(IDbConnection conn)
+        {
+            this._conn = conn;
+        }
+
+        public IActionResult CloseYieldbyDay()
+        {
+            return View();
+        }
+
+
+        public IActionResult CloseYieldbyLot()
+        {
+            return View();
+        }
+        public IActionResult DailyDefect()
+        {
+            return View();
+        }
+        public IActionResult DailyYield()
+        {
+            List<DailyYieldViewModel> vm = new List<DailyYieldViewModel>();
+
+
+
+            return View();
+        }
+        public IActionResult LowYieldSetting()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ YieldQuerySystem/Models/ViewModel/DailyYieldViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YieldQuerySystem.Models.ViewModel
+{
+    public class DailyYieldViewModel
+    {
+        string StageCode { get; set; }
+
+        int InQty { get; set; }
+
+        int OutQty { get; set; }
+
+        int DefectQty { get; set; }
+
+        string Yield { get; set; }
+
+        string OutTime { get; set; }
+
+    }
+}
