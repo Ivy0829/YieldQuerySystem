802cc591819c70737fb0227ca02fbbdd7351935c
sses40417 <sses40417@gmail.com>
2022-08-07 19:26:08 +0800

    資料庫專案建置

---
+++ YieldReportSyatem/dbo/Stored Procedures/SP_DailyYieldByStage.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		<Ivy>
+-- Create date: <2022/07/21>
+-- Description:	<Daily Yield by Stage Data>
+-- =============================================
+CREATE PROCEDURE [dbo].[SP_DailyYieldByStage]
+	-- Add the parameters for the stored procedure here
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	SELECT StageCode,sum(TrackInQty) as InQty, sum(TrackOutQty) as OutQty,sum(TrackInQty)-sum(TrackOutQty) as DefectQty,Convert(varchar,CONVERT(decimal(18,4),sum(TrackOutQty)*1.0/sum(TrackInQty)*100))+ '%' as Yield,CONVERT(varchar(100),TrackOutTime,111) as OutTime  
+FROM DailyYield as dy
+group by StageCode,CONVERT(varchar(100),TrackOutTime,111)
+order by OutTime
+
+END
---
+++ YieldReportSyatem/dbo/Tables/DailyYieldDetail.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[DailyYieldDetail] (
+    [Guid]       VARCHAR (64)  NOT NULL,
+    [DefectName] VARCHAR (100) NOT NULL,
+    [DefectQty]  INT           NOT NULL
+);
+
